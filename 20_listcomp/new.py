83da85e17b7a0c4e34404dca7cb58d957394c8ca
Shafali Gupta <sgupta@stuy.edu>
2019-04-18 07:07:55 -0400

    done

---
+++ 20_listcomp/new.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python3
+# RASHA:
+import sys
+from functools import reduce
+
+def main(file):
+    infile= open(file,'r')
+    lines= infile.read().split('\n')# infile is now a list of lines
+    lines = join
+
+def findFreq(word):
+    lst= main('text.txt')
+    return reduce(lambda x,y: print(type(x)), lst)
+
+print(findFreq("hi"))

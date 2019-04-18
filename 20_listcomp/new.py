edb3d7b4a4d1b337381da72a306ac27cf2291bde
Shafali Gupta <sgupta@stuy.edu>
2019-04-18 13:54:43 -0400

    updates

--- 20_listcomp/new.py
+++ 20_listcomp/new.py
@@ -1,15 +1,31 @@
-#! /usr/bin/python3
-# RASHA:
-import sys
+#Team Jellyfish -- Shafali Gupta & Matthew Ming
+#SoftDev02Pd08
+#K20 -- Reductio ad Absurdum
+#2019-04-17
+
 from functools import reduce
 
-def main(file):
-    infile= open(file,'r')
-    lines= infile.read().split('\n')# infile is now a list of lines
-    lines = join
-
-def findFreq(word):
-    lst= main('text.txt')
-    return reduce(lambda x,y: print(type(x)), lst)
-
-print(findFreq("hi"))
+file = open("16.txt",'r')
+a=[j for x in file.readlines()for j in x.split()]
+##print(a)
+def freq_single(n):
+    b=[x for x in a if x==n]
+    return len(b)
+print(freq_single("the"))#Expecting 280
+print(freq_single("a"))#Expecting 121
+def freq_mult(g):
+    b=reduce(lambda i,j: i+" "+j,a)
+    #print(b)
+    c = [x for x in range(len(b)-len(g)) if b[x:x+len(g)] == g]  
+    return len(c)
+print(freq_mult("under the"))#Expecting 5
+def freq():
+    z=[len([y for y in a if y==x]) for x in a]
+    return a[z.index(max(z))]
+print(freq())#Expecting the
+##No need to use reduce to find frequecy mult or singular
+#wow reduce functions the same as max except its longer!!
+def red_freq():
+    z=[len([y for y in a if y==x]) for x in a]
+    return a[z.index(reduce(lambda x,y,: x if x > y else y,z))]
+print(red_freq())#Expecting the
